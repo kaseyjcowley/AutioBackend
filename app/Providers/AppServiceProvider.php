d0531839c93a1e8948478ee293fffeb4143a5214
Kasey Cowley <kchunterdeluxe@gmail.com>
2015-09-10 23:18:18 -0600

    Removes Larcasts/Generators from Providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,9 +8,7 @@ class AppServiceProvider extends ServiceProvider {
      *
      * @return void
      */
-    public function register() {
-      if ($this->app->environment() === 'local') {
-        $this->app->register('Laracasts\Generators\GeneratorsServiceProvider');
-      }
+    public function register() 
+    {
     }
 }
